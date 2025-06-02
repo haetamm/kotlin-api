ce69e274f8a723920934fc10644fbc538b8224df
Thaetami <tatanghaetami.97@gmail.com>
2025-06-02 20:44:50 +0700

    feat: add table seeder

---
+++ src/main/kotlin/com/belajar/api/kotlin/seeder/TableSeeder.kt
@@ -0,0 +1,35 @@
+package com.belajar.api.kotlin.seeder
+
+import com.belajar.api.kotlin.model.TableRest
+import com.belajar.api.kotlin.repository.TableRepository
+import org.springframework.boot.CommandLineRunner
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+
+@Order(3)
+@Component
+class TableSeeder(
+    private val tableRepository: TableRepository
+) : CommandLineRunner {
+
+    @Transactional
+    override fun run(vararg args: String?) {
+        val names = listOf("A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10")
+        val existingNames = tableRepository.findAll()
+            .map { it.name }
+
+        val newTables = names
+            .filterNot { existingNames.contains(it) }
+            .map { TableRest(name = it) }
+
+        if (newTables.isNotEmpty()) {
+            tableRepository.saveAll(newTables)
+            println("✅ Category seeding complete.")
+        } else {
+            println("ℹ️ Category data already seeded.")
+        }
+    }
+
+}
